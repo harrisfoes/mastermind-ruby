7bdeece00a6fced0084d9612a71c169fdb1399f2
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-03-03 13:13:25 +0900

    finished single player mode

--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ puts "Can you break the code?"
 
 CODE_ELEMENTS = ['r','g','b','y'].freeze
 CODE_LENGTH = 4
-TRIES = 4
+TRIES_LIMIT = 10 
+CORRECT_GUESS = 'XXXX'.freeze
+
 
 def generate_code()
   randy = []
@@ -11,6 +13,66 @@ def generate_code()
   randy
 end
 
-p generate_code()
+def evaluate_guess(guess, answer)
+
+  result = []
+  guess_used = Array.new(guess.length, false) 
+  answer_used = Array.new(answer.length, false)
+
+  guess.chars.each_with_index do | ch, idx |
+    if ch == answer[idx]
+      result << 'X'
+      guess_used[idx] = true 
+      answer_used[idx] = true
+    end
+  end
+  result
+
+  guess.chars.each_with_index do | guess, idx|
+    next if guess_used[idx]
+    
+    answer.each_with_index do | ans, jdx|
+      if !answer_used[jdx] && guess == ans
+        result << 'x'
+        answer_used[jdx] = true
+        break
+      end
+    end 
+  end
+  result
+end
+
+def guess_correct?(eval)
+  eval == 'XXXX' ? true : false 
+end
+
+puts "This is the secret code"
+secret_code =  generate_code()
+#p secret_code
+
+tries = 1 
+eval = ''
+
+until tries == TRIES_LIMIT or guess_correct?(eval) do
+  p "Your guess: "
+  guess = gets.chomp
+  
+  eval = evaluate_guess(guess, secret_code).join("")
+
+  p "Guess number #{tries}, guess: #{guess} result: #{eval}"
+
+  tries += 1
+end
+
+if guess_correct?(eval)
+  p "You cracked the code!"
+elsif
+  p "You didn't crack the code! "
+end
+
+p "The answer is #{secret_code.join("")}"
+
+
+
 
 
