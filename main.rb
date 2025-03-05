17f997872dff6b3805b439c99fa54fa572c548df
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-03-05 15:44:27 +0900

    refactor for when user is codebreaker

--- main.rb
+++ main.rb
@@ -45,28 +45,39 @@ def guess_correct?(eval)
 end
 
 # game section
-secret_code =  generate_code()
-tries = 1 
-eval = ''
 
-until tries == TRIES_LIMIT or guess_correct?(eval) do
-  p "Your guess: "
-  guess = gets.chomp
-  
-  eval = evaluate_guess(guess, secret_code)
+def user_plays()
+  secret_code =  generate_code()
+  tries = 1 
+  eval = ''
 
-  p "Guess number #{tries}, guess: #{guess} result: #{eval}"
+  puts "You are now the CODEBREAKER"
+  puts "The valid colors are r,g,b,y,m,c"
+  puts "The code is a 4-COLOR combination of the above"
+  puts "Your guess will be evaluated as follows: "
+  puts "X - if color and it's position is correct"
+  puts "x - if color is correct and it's position is wrong"
+  puts "You have #{TRIES_LIMIT} tries"
 
-  tries += 1
+  until tries == TRIES_LIMIT or guess_correct?(eval) do
+    p "Your guess: "
+    guess = gets.chomp
+    
+    eval = evaluate_guess(guess, secret_code)
+
+    p "Guess number #{tries}, guess: #{guess} result: #{eval}"
+
+    tries += 1
+  end
+
+  if guess_correct?(eval)
+    p "You cracked the code!"
+  elsif
+    p "You didn't crack the code! "
+  end
+
+  p "The answer is #{secret_code.join("")}"
 end
-
-if guess_correct?(eval)
-  p "You cracked the code!"
-elsif
-  p "You didn't crack the code! "
-end
-
-p "The answer is #{secret_code.join("")}"
 
 #TODO, store guesses tries and results in a hash
 #refactor to enable computer player
@@ -80,3 +91,35 @@ p "The answer is #{secret_code.join("")}"
 #   player will supply guesses
 #
 #   keep it simple!
+
+def get_valid_input()
+  loop do
+    print "What's your choice?: "
+    choice = gets.chomp[0]
+
+    return choice if choice == '1' or choice == '2' 
+
+    puts 'Please enter 1 or 2'
+
+    end
+end
+
+#Intro menu
+puts "This is Mastermind, a game of cracking codes"
+puts "Please select how you'd like to play"
+puts "[1] I want to be the codebreaker"
+puts "[2] I want to be the codemaker"
+
+choice = get_valid_input()
+puts "Your choice is #{choice}"
+
+#game start
+if choice.to_i == 1
+  user_plays() 
+end
+
+
+
+
+
+
