cf84eada5de60c03c055bbe9353a69d380a3e9fe
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-03-08 19:41:53 +0900

    classified pc plays

--- mastermind.rb
+++ mastermind.rb
@@ -92,21 +92,21 @@ class User_Plays_MM < Mastermind
   end
 
   def start()
-    self.prompt()
-    self.get_all_guesses()
-    self.final_result()
+    prompt()
+    get_all_guesses()
+    final_result()
   end
 
   def get_all_guesses()
-    until @tries == Mastermind::TRIES_LIMIT or guess_correct? do
+    until @tries == Mastermind::TRIES_LIMIT + 1 or guess_correct? do
 
       if @game_records.length > 0
-        self.display()
+        display()
       end
 
       guess = get_player_guess()
       eval = evaluate_guess(guess.chars, @secret_code)
-      self.add_game_record(@tries, guess, eval)
+      add_game_record(@tries, guess, eval)
 
       @tries += 1
     end
@@ -125,6 +125,7 @@ class User_Plays_MM < Mastermind
   end
 
   def final_result()
+    display()
     if guess_correct?
       puts "You cracked the code!"
     elsif
@@ -137,6 +138,60 @@ class User_Plays_MM < Mastermind
 
 end
 
-game2 = User_Plays_MM.new
-game2.start()
 
+#game2 = User_Plays_MM.new🤣
+#game2.start()
+
+class PC_Plays_MM < Mastermind
+  def initialize()
+    super
+  end
+
+  def prompt()
+    super
+    puts "You will supply the code and the computer will try to guess"
+    puts "In #{Mastermind::TRIES_LIMIT} tries"
+  end
+
+  def start()
+    @secret_code = get_player_made_code().chars
+
+    generate_guesses()
+    display()
+    final_result()
+  end
+
+  def get_player_made_code()
+    loop do
+      print "What is the code?: "
+      secret_code = gets.chomp()
+
+      return secret_code if secret_code.match?(/^[rgbymc]{4}$/)
+
+      puts "Please select a four-letter combination of [r,g,b,y,m,c]"
+    end
+  end
+
+  def generate_guesses()
+    until tries == Mastermind::TRIES_LIMIT + 1 or guess_correct? do
+      computer_guess = generate_code()
+      eval = evaluate_guess(computer_guess, secret_code)
+      add_game_record(@tries, computer_guess.join, eval)
+      @tries += 1
+    end
+  end
+
+  def final_result()
+    if guess_correct?
+      p "The computer cracked the code!"
+    elsif
+      p "The computer didn't crack the code! "
+    end
+
+    puts "The code is #{@secret_code.join}"
+  end
+
+end
+
+game3 = PC_Plays_MM.new
+game3.start()
