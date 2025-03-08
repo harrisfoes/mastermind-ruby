91f89aec3f0a0412a0958c64458792d6c589a5c6
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-03-08 18:07:50 +0900

    finished classifying USER_PLAYES_MM

--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,5 @@
 class Mastermind
+  attr_accessor :secret_code, :tries
 
   CODE_ELEMENTS = ['r','g','b','y','m','c'].freeze
   CODE_LENGTH = 4
@@ -7,8 +8,10 @@ class Mastermind
 
   def initialize()
     @secret_code = []
-    @tries = 0
-    @guess = []
+    @tries = 1
+
+    ## Hash with {tries:1 , guess: rrby eval: => XXx}
+    @game_records = []
 
   end
 
@@ -18,13 +21,20 @@ class Mastermind
     randy
   end
 
+  def prompt()
+    puts "You are now the CODEBREAKER"
+    puts "The valid colors are r,g,b,y,m,c"
+    puts "The code is a 4-COLOR combination of the above"
+  end
+
   def evaluate_guess(guess, answer)
+    #answer and guess should be arrays
 
     result = []
     guess_used = Array.new(guess.length, false) 
     answer_used = Array.new(answer.length, false)
 
-    guess.chars.each_with_index do | ch, idx |
+    guess.each_with_index do | ch, idx |
       if ch == answer[idx]
         result << 'X'
         guess_used[idx] = true 
@@ -32,7 +42,7 @@ class Mastermind
       end
     end
 
-    guess.chars.each_with_index do | guess, idx|
+    guess.each_with_index do | guess, idx|
       next if guess_used[idx]
       
       answer.each_with_index do | ans, jdx|
@@ -46,16 +56,87 @@ class Mastermind
     result.join("")
   end
 
-  def guess_correct?(eval)
-    eval == CORRECT_GUESS ? true : false
+  def guess_correct?()
+    if @game_records.length > 0
+      @game_records.last[:eval] == CORRECT_GUESS ? true : false
+    end
   end
 
   def display()
-    #TODO display the guesses, tries and eval so far
+    @game_records.each do | record | 
+      p "Try number: # #{record[:try]} is #{record[:guess]}, eval: #{record[:eval]}"
+    end
   end
 
-  def add_guess()
-    #TODO add a guess, maybe it can be added to guess then => eval 
+  def add_game_record(try, guess, eval)
+    @game_records << { try: try, guess:  guess, eval: eval }
   end
 
 end
+
+
+class User_Plays_MM < Mastermind
+
+  def initialize()
+    super
+
+    @secret_code = generate_code() 
+  end
+
+  def prompt()
+    super  
+    puts "Your guess will be evaluated as follows: "
+    puts "X - if color and it's position is correct"
+    puts "x - if color is correct and it's position is wrong"
+    puts "You have #{Mastermind::TRIES_LIMIT} tries"
+  end
+
+  def start()
+    self.prompt()
+    self.get_all_guesses()
+    self.final_result()
+  end
+
+  def get_all_guesses()
+    until @tries == Mastermind::TRIES_LIMIT or guess_correct? do
+
+      if @game_records.length > 0
+        self.display()
+      end
+
+      guess = get_player_guess()
+      eval = evaluate_guess(guess.chars, @secret_code)
+      self.add_game_record(@tries, guess, eval)
+
+      @tries += 1
+    end
+  end
+
+  def get_player_guess()
+    loop do
+      print "What is your guess?: "
+      guess = gets.chomp()
+
+      return guess if guess.match?(/^[rgbymc]{4}$/)
+
+      puts "Please select a four-letter combination of [r,g,b,y,m,c]"
+    end
+
+  end
+
+  def final_result()
+    if guess_correct?
+      puts "You cracked the code!"
+    elsif
+      puts "You didn't crack it!"
+    end
+
+    puts "The code is #{@secret_code.join("")}"
+  end
+
+
+end
+
+game2 = User_Plays_MM.new
+game2.start()
+
